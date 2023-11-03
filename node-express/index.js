1f965547fd3096739e6787aed7e0439086b937cf
Yair <yair.tal3@gmail.com>
2023-11-03 23:31:20 +0200

    change index.js

--- node-express/index.js
+++ node-express/index.js
@@ -1,34 +1,18 @@
 const express = require('express');
 const cors = require('cors');
-const app=express()
+require("./utils/db");
+const routes = require("./routes");
 
+const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors({
-  origin: 'http://localhost:3000'
-}));
 
-app.get("/items", (req, res) => {
-    res.send([{
-        title: "Check this out",
-        text: "Our new hamburger is ready!",
-        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Hamburger_%28black_bg%29.jpg/500px-Hamburger_%28black_bg%29.jpg",
-        link: {
-          text: "Order Now!",
-          path: "/order"
-        }
-      },
-      {
-        title: "Did someone say VEGAN?",
-        text: "We also have a vegan Option, just check out our vegan options!",
-        image:"https://images.everydayhealth.com/images/what-is-a-vegan-diet-benefits-food-list-beginners-guide-alt-1440x810.jpg?w=1110",
-        link: {
-          text: "I'm Vegan",
-          path: "/order"
-        }
-      }]);
-});
+app.use("/", routes);
 
-
-app.listen(8081, () => console.log('Server started'));
+app.listen(8081, () => console.log("Server started on port 8081"));
